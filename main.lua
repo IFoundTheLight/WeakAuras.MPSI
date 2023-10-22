7992e9b5400dd05018270fcdcf2812239f830cd2
Robert Fern II <IFoundTheLight@gmail.com>
2023-10-22 16:45:29 -0400

    Initial

---
+++ main.lua
@@ -0,0 +1,120 @@
+{
+    ["controlledChildren"] = {
+        [1] = "Information Window",
+        [2] = "Button",
+    },
+    ["borderBackdrop"] = "Solid",
+    ["wagoID"] = "hCxc3HSH9",
+    ["authorOptions"] = {
+    },
+    ["preferToUpdate"] = false,
+    ["groupIcon"] = 134235,
+    ["anchorPoint"] = "RIGHT",
+    ["borderColor"] = {
+        [1] = 1,
+        [2] = 1,
+        [3] = 1,
+        [4] = 1,
+    },
+    ["url"] = "https://wago.io/hCxc3HSH9",
+    ["actions"] = {
+        ["start"] = {
+        },
+        ["init"] = {
+        },
+        ["finish"] = {
+        },
+    },
+    ["triggers"] = {
+        [1] = {
+            ["trigger"] = {
+                ["type"] = "aura2",
+                ["subeventSuffix"] = "_CAST_START",
+                ["event"] = "Health",
+                ["unit"] = "player",
+                ["spellIds"] = {
+                },
+                ["subeventPrefix"] = "SPELL",
+                ["names"] = {
+                },
+                ["debuffType"] = "HELPFUL",
+            },
+            ["untrigger"] = {
+            },
+        },
+    },
+    ["internalVersion"] = 68,
+    ["animation"] = {
+        ["start"] = {
+            ["type"] = "none",
+            ["easeStrength"] = 3,
+            ["duration_type"] = "seconds",
+            ["easeType"] = "none",
+        },
+        ["main"] = {
+            ["type"] = "none",
+            ["easeStrength"] = 3,
+            ["duration_type"] = "seconds",
+            ["easeType"] = "none",
+        },
+        ["finish"] = {
+            ["type"] = "none",
+            ["easeStrength"] = 3,
+            ["duration_type"] = "seconds",
+            ["easeType"] = "none",
+        },
+    },
+    ["desc"] = "This displays information about the key and vault drops for the season.",
+    ["version"] = 1,
+    ["subRegions"] = {
+    },
+    ["load"] = {
+        ["talent"] = {
+            ["multi"] = {
+            },
+        },
+        ["spec"] = {
+            ["multi"] = {
+            },
+        },
+        ["class"] = {
+            ["multi"] = {
+            },
+        },
+        ["size"] = {
+            ["multi"] = {
+            },
+        },
+    },
+    ["backdropColor"] = {
+        [1] = 0,
+        [2] = 0,
+        [3] = 0,
+        [4] = 0,
+    },
+    ["source"] = "import",
+    ["scale"] = 1,
+    ["border"] = false,
+    ["borderEdge"] = "Blizzard Dialog Gold",
+    ["regionType"] = "group",
+    ["borderSize"] = 20,
+    ["anchorFrameParent"] = true,
+    ["borderInset"] = 1,
+    ["borderOffset"] = 12,
+    ["semver"] = "1.0.0",
+    ["tocversion"] = 100107,
+    ["id"] = "Mythic+ Season Info",
+    ["selfPoint"] = "CENTER",
+    ["frameStrata"] = 2,
+    ["anchorFrameType"] = "SELECTFRAME",
+    ["yOffset"] = 0,
+    ["config"] = {
+    },
+    ["uid"] = "KjImmZ3vujV",
+    ["anchorFrameFrame"] = "PVEFrame",
+    ["conditions"] = {
+    },
+    ["information"] = {
+    },
+    ["xOffset"] = 35,
+}
